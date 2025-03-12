af8e7679a77fbbae93068e1a9be68eb2f8d6701d
qwuser234 <qwrocket39@gmail.com>
2025-03-12 19:26:13 +0200

    opensource

---
+++ src/krnl/krnl.c
@@ -0,0 +1,30 @@
+#include "stdint.h"
+#include "time.h"
+
+char* vidptr = (char*)0xB8000;
+
+void print(const char* str) {
+    unsigned int j = 0, i = 0;
+    while (str[j] != '\0'){
+        vidptr[i] = str[j];
+        vidptr[i+1] = 0x07;
+        i = i + 2;
+        j++;
+    }
+}
+void clear(void){
+    unsigned int j = 0;
+    while(j < 80*25*2){
+        vidptr[j] = '\0';
+        vidptr[j+1] = 0x07;
+        j++;
+    }
+}
+
+void kernel_main() {
+    clear();
+    print("ShilOS is run");
+    while (1) {
+        __asm__("hlt");
+    }
+}
